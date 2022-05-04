7a855e5462d59a2c822af7ae987bd26d9f354843
Lluissb <97342518+Lluissb@users.noreply.github.com>
2022-05-04 19:33:39 +0200

    Delete project1.js

--- project1.js
+++
@@ -1,7 +0,0 @@
-function mobileMenu() {
-    if (document.querySelector(".nav").style.display === "block") {
-        document.querySelector(".nav").style.display = "none";
-    } else { document.querySelector(".nav").style.display = "block";
-    }
-}
-document.querySelector(".burger-menu").addEventListener("click", mobileMenu);
