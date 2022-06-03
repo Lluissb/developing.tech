f527b72c48a79e7abf0323fe537a75ba951cf50d
Lluissb <97342518+Lluissb@users.noreply.github.com>
2022-06-03 13:07:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+$('#project1').on('click', () => {
+    $('#text1').toggle();
+});
+
+$('#project2').on('click', () => {
+    $('#text2').toggle();
+});
+
+$('#project3').on('click', () => {
+    $('#text3').toggle();
+});
+
+$('.material-symbols-outlined').on('click', () =>{
+    $('.menu-nav').toggle();
+});
+
+$('.nav__link').on('click', () => {
+        $('.menu-nav').hide();
+});
